23d32d6f9800701f564a39939f9bf955b1d6f11e
zhangpeng <670287431@qq.com>
2016-10-30 22:08:22 +0800

    324

--- src/com/csai/db/DBConn.java
+++ src/com/csai/db/DBConn.java
@@ -1,10 +1,13 @@
 package com.csai.db;
 import java.sql.Connection;
 import java.sql.DriverManager;
+
+import org.apache.jasper.tagplugins.jstl.core.Out;
 public class DBConn {
 	public static Connection createConn() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
+			String string=null;
 			Connection conn=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/StudyTest?useUnicode=true&amp;characterEncoding=UTF-8","root","asdf1234");
 			return conn;
 		} catch (Exception e) {
