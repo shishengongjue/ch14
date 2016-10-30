db31404b53d2186ef4308835da6c2f99a1a86996
zhangpeng <670287431@qq.com>
2016-10-30 21:51:14 +0800

    1test

--- src/com/csai/action/LoginAction.java
+++
@@ -1,5 +0,0 @@
-package com.csai.action;
-
-public class LoginAction {
-
-}
--- src/com/csai/db/DBConn.java
+++
@@ -1,6 +0,0 @@
-package com.csai.db;
-import java.sql.Connection;
-import java.sql.DriverManager;
-public class DBConn {
-	
-}
