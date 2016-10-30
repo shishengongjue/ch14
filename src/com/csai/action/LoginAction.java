347f9757359e9b1c952dfddba287a3acb88e5878
zhangpeng <670287431@qq.com>
2016-10-30 21:27:59 +0800

    1、添加mysql都jdbc

--- src/com/csai/action/LoginAction.java
+++ src/com/csai/action/LoginAction.java
@@ -1,5 +1,66 @@
 package com.csai.action;
-
-public class LoginAction {
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import com.csai.db.DBConn;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+public class LoginAction extends ActionSupport{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public String adminusername;
+	public String getAdminusername() {
+		return adminusername;
+	}
+	public void setAdminusername(String adminusername) {
+		this.adminusername = adminusername;
+	}
+	public String getAdminuserpassword() {
+		return adminuserpassword;
+	}
+	public void setAdminuserpassword(String adminuserpassword) {
+		this.adminuserpassword = adminuserpassword;
+	}
+	public String getAction() {
+		return action;
+	}
+	public void setAction(String action) {
+		this.action = action;
+	}
+	public String getErrormsg() {
+		return errormsg;
+	}
+	public void setErrormsg(String errormsg) {
+		this.errormsg = errormsg;
+	}
+	public String adminuserpassword;
+	public String action;
+	public String errormsg;
+	
+	@Override
+	public String execute() throws Exception{
+		if("login".equals(action)){
+			try {
+				Connection conn=DBConn.createConn();
+				String sql="Select * from adminuser where adminusername=? and adminuserpassword=?";
+				PreparedStatement statement=conn.prepareStatement(sql);
+				statement.setString(1, adminusername);
+				statement.setString(2, adminuserpassword);
+				ResultSet rs=statement.executeQuery();
+				if(rs.next()){
+					ActionContext.getContext().getSession().put("adminusername", adminusername);
+					ActionContext.getContext().getSession().put("adminuserpassword", adminuserpassword);
+					ActionContext.getContext().getSession().put("adminuserrole", rs.getString("adminuserrole"));
+					DBConn.closeConn(conn);
+					return SUCCESS;
+				}
+			} catch (Exception e) {
+				errormsg=new String("数据库连接有误！");
+			}
+		}
+		return INPUT;
+	}
 }
--- src/com/csai/db/DBConn.java
+++ src/com/csai/db/DBConn.java
@@ -2,5 +2,22 @@ package com.csai.db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 public class DBConn {
+	public static Connection createConn() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/StudyTest?useUnicode=true&amp;characterEncoding=UTF-8","root","asdf1234");
+			return conn;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 	
+	public static void closeConn(Connection conn) {
+		try {
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
